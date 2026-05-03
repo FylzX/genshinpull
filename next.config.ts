7a88e4001d269289e2f0ebfbf2f0c169735bcb78
Leon Cai <FylzX>
2026-05-03 20:25:10 +0800

    1

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export', // 必须添加这一行
+  images: {
+    unoptimized: true, // 静态导出必须禁用 Next.js 的默认图片优化
+  },
 };
 
 export default nextConfig;
