a789d6afc7a4a8236072f66194a684369f0d8845
FylzX <187797805+FylzX@users.noreply.github.com>
2026-05-03 20:39:10 +0800

    fix: rename config to mjs to ensure Cloudflare picks up export mode

--- next.config.ts
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export', // 必须添加这一行
-  images: {
-    unoptimized: true, // 静态导出必须禁用 Next.js 的默认图片优化
-  },
-};
-
-export default nextConfig;
